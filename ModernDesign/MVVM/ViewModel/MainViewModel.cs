5e776786501c454c7a28a7222e9763acaa9480aa
anupam kumar <annupam2003@live.com>
2022-03-22 09:35:24 +0530

    Add project files.

---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObsservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryVeiwModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryVeiwModel();
+            CurrentView = HomeVM;
+
+           HomeViewCommand = new RelayCommand(obj => { CurrentView = HomeVM; });
+            DiscoveryViewCommand = new RelayCommand(obj => { CurrentView = DiscoveryVM; });
+        }
+    }
+}
